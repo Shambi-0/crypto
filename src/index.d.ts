b202a059a512bc323513b9cf29ef95f072926949
Shambi <87273502+Shambi-0@users.noreply.github.com>
2023-04-12 11:16:15 -0500

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,19 @@
+/// <reference types="@rbxts/types"/>
+
 interface KeyPair {
-    Public: string;
-    Secret: string;
+    Public: string,
+    Secret: string
 }
 
-interface Curve25519 {
-    GenerateKeyPair(): KeyPair;
-    GenerateSessionKey(Sender: string, Recipient: string): string;
+declare namespace Curve25519 {
+    function GenerateKeyPair(): KeyPair;
+    function GenerateSessionKey(Sender: string, Recipient: string): string;
 }
 
-interface AES {
-    GenerateKey(): string;
-    Encrypt(Data: string, Key: string): string;
-    Decrypt(Data: string, Key: string): string;
+declare namespace AES {
+    function GenerateKey(): string;
+    function Encrypt(Data: string, Key: string): string;
+    function Decrypt(Data: string, Key: string): string;
 }
-export default interface crypto {
-    EC25519: Curve25519;
-    AES: AES;
-}
+
+export { Curve25519, AES };
