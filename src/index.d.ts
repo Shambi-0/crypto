cdd31f4780fb86fad805d0b2bec7dbae0d27cb1a
Shambi <87273502+Shambi-0@users.noreply.github.com>
2023-04-12 10:52:11 -0500

    AES

--- src/index.d.ts
+++ src/index.d.ts
@@ -8,6 +8,12 @@ interface Curve25519 {
     GenerateSessionKey(Sender: string, Recipient: string): string;
 }
 
+interface AES {
+    GenerateKey(): string;
+    Encrypt(Data: string, Key: string): string;
+    Decrypt(Data: string, Key: string): string;
+}
 export default interface crypto {
     EC25519: Curve25519;
+    AES: AES;
 }
--- src/index.lua
+++ src/index.lua
@@ -1,5 +1,10 @@
 local Curve25519 = require(script:WaitForChild("Curve25519"));
 
+local Modules = script:WaitForChild("Modules");
+
+local AES = require(Modules:WaitForChild("AES"));
+
 return {
     ["EC25519"] = Curve25519;
+    ["AES"] = AES;
 };
